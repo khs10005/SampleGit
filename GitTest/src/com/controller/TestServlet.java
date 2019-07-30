a4ac54d7be21e0f80baceae51710fb6290ae3d4f
khs10005 <khs10005@naver.com>
2019-07-30 10:17:49 +0900

    hello4 insert

--- GitTest/src/com/controller/TestServlet.java
+++ GitTest/src/com/controller/TestServlet.java
@@ -17,6 +17,7 @@ public class TestServlet extends HttpServlet {
 		System.out.println("Hello1");
 		System.out.println("Hello2");
 		System.out.println("Hello3");
+		System.out.println("Hello4");
 		
 	}
 
